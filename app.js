e538467e74b25627f1ad1bba234339d182a9c894
prakashranjan <prakashranjansingh04@gmail.com>
2018-07-22 13:54:49 +0530

    user register works

--- app.js
+++ app.js
@@ -4,15 +4,51 @@ const bodyParser=require("body-parser");
 const cors=require("cors");
 const passport=require("passport");
 const mongoose=require("mongoose");
+const config = require("./config/database");
+
+//connect database
+mongoose.connect(config.database,{useNewUrlParser: true});
+
+//on connection
+mongoose.connection.on("connected" , () => {
+    console.log(" connected to database "+config.database);
+});
+
+//on error
+mongoose.connection.on("error" , (err) => {
+
+    console.log("error is "+ err);
+});
 
 const app = express();
 
+const users = require("./routes/users");
+
+//port number
 const port = 3000;
 
+//CORS middleware
+app.use(cors());
+
+//set static folder 
+app.use(express.static(path.join(__dirname, "public")));
+
+//body-parser middleware
+app.use(bodyParser.json());
+
+//passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use("/users", users);
+
+
+//index route
 app.get( "/" , (req,res) => {
     res.send("Invalid Endpoint");
 });
 
+//start server
 app.listen( port , () => {
     console.log("server started on port "+port);
 });
